326860d9b14ccbb1e2d9a321302c05b309253bd4
Yusuf Farhan <yusuf.hasbullah@misteraladin.com>
2022-08-23 20:39:53 +0700

    Task 2

---
+++ Task-2/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	friend := []string{
+		"Giva",
+		"Fahmi",
+		"Irfan",
+		"Hasanuddin",
+		"Fiqri",
+		"Ricky",
+		"Nugraha",
+		"Talia",
+		"Clara",
+		"Bayu",
+		"Eka",
+	}
+
+	for _, v := range friend {
+		fmt.Println(v)
+	}
+}
